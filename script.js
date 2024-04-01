817d9d65a60477cbef28095b7fe50dda4e0467fb
saqibmd <67945382+saqibmd@users.noreply.github.com>
2024-04-01 22:01:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,136 @@
+function homePageAnimation(){
+    gsap.set(".scaler", {scale: 5})
+
+var tl = gsap.timeline({
+    scrollTrigger: {
+        trigger : ".home",
+        start : "top top",
+        end : "bottom bottom",
+        scrub : 2
+}
+
+});
+
+tl.to(".vdodiv",{
+    '--clip': "0%",
+    ease : Power2
+    }, 'a')
+
+ tl.to(".scaler", {
+    scale : 1,
+    ease : Power2,
+ }, 'a');
+
+
+tl.to(".lft", {
+    xPercent : -10,
+    stagger: .03,
+    ease : Power4
+},'b')
+
+tl.to(".rgt", {
+    xPercent : 10,
+    stagger: .03,
+    ease : Power4
+},'b')
+}
+
+function realPageAnimation(){
+    gsap.to(".slide", {
+        scrollTrigger : {
+            trigger: ".real",
+            start: "top top",
+            end: "bottom bottom",
+            scrub:2
+        },
+        xPercent: -200,
+        ease: Power4
+    })
+}
+
+function teamAnimation(){
+    document.querySelectorAll(".listelem").forEach(function(el){
+        el.addEventListener("mousemove", function(dets){
+            gsap.to(this.querySelector(".picture"),{
+                opacity : 1,
+                x: gsap.utils.mapRange(0, window.innerWidth, -200, 200, dets.clientX),
+                ease: Power4,
+                duration : 0.5,
+            });
+        });
+        el.addEventListener("mouseleave", function(dets){
+            gsap.to(this.querySelector(".picture"),{
+                opacity : 0,
+                ease: Power4,
+                duration: 0.5
+            });
+        });
+    });
+    
+}
+
+function paraAnimation(){
+    let clutter = "";
+document.querySelector(".textpara").textContent.split("").forEach(function(e){
+    if(e === " ") clutter += `<span>&nbsp;</span>`
+    clutter +=`<span>${e}</span>`
+})
+
+document.querySelector(".textpara").innerHTML = clutter;
+
+gsap.set(".textpara span", {opacity: .1})
+gsap.to(".textpara span", {
+    scrollTrigger :{
+        trigger: ".para",
+        start: "top 60%",
+        end: "bottom 90%",
+        scrub : 1,
+    },
+    opacity : 1,
+    stagger: .03,
+    ease : Power4
+})
+
+}
+
+function loco(){
+    (function () {
+        const locomotiveScroll = new LocomotiveScroll();
+    })();
+}
+
+function capsule(){
+    gsap.to(".capsule", {
+        scrollTrigger: {
+            trigger: ".capsules",
+            start: "top 70%",
+            end: "bottom bottom",
+            scrub: 1,
+        },
+        y: 0,
+        ease: Power4
+    })
+}
+
+
+document.querySelectorAll(".section").forEach(function(e){
+    ScrollTrigger.create({
+        trigger: e,
+        start: "top 50%",
+        end: "bottom 50%",
+        // scrub: 2
+        onEnter: function(){
+            document.body.setAttribute("theme", e.dataset.color);
+        },
+        onEnterBack: function(){
+            document.body.setAttribute("theme", e.dataset.color);
+        }
+    })
+})
+
+loco()
+homePageAnimation()
+realPageAnimation()
+teamAnimation()
+paraAnimation()
+capsule();
